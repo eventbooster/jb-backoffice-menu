a936291dbf51d18e2d6a148f8cf0f0e5e6a420e2
Felix Steiner <felix_steiner@bluewin.ch>
2015-07-01 19:20:53 +0200

    fixed submenu display if we're already in a submenu

--- src/jb-backoffice-menu.js
+++ src/jb-backoffice-menu.js
@@ -14,7 +14,20 @@ angular
 
 } ] )
 
-.directive( 'backofficeMenu', [ function() {
+.directive( 'backofficeMenu', [ '$location', function( $location ) {
+
+	function containsChild( item, name ) {
+
+		if( item.children && item.children.length ) {
+			return item.children.some( function( child ) {
+				if( child.entity === name ) {
+					return true;
+				}
+			} );
+		}
+		return false;
+
+	}
 
 	return {
 		templateUrl		: 'backofficeMenuTemplate.html'
@@ -22,6 +35,22 @@ angular
 		, link			: function( scope, element, attrs, ctrl ) {
 			
 			ctrl.init( element );
+
+			scope.showSubmenu = function( item ) {
+
+				// Current entity, without leading /
+				var currentEntity = $location.path().split( '/' )[ 1 ];
+
+				if( currentEntity === item.entity ) {
+					return true;
+				}
+
+				if( containsChild( item, currentEntity ) ) {
+					return true;
+				}
+
+				return false;
+			};
 
 		}
 		, scope			: {
@@ -40,7 +69,7 @@ angular
 					'<span class=\'menu-icon\'><i class=\'fa\' data-ng-class=\'item.icon\'></i></span>' +
 					'<span class=\'menu-text\' data-translate=\'web.backoffice.menu.{{ item.entity }}\'></span>' +
 				'</a>' +
-				'<ul class=\'child\' data-ng-repeat=\'subItem in item.children\' data-ng-hide=\'currentEntity !== item.entity\'>' +
+				'<ul class=\'child\' data-ng-repeat=\'subItem in item.children\' data-ng-show=\'showSubmenu( item )\'>' +
 					'<li><a data-ng-attr-href=\'#{{ subItem.entity }}\' data-translate=\'web.backoffice.menu.{{ subItem.entity }}\'></a></li>' +
 				'</ul>' +
 			'</li>' +
